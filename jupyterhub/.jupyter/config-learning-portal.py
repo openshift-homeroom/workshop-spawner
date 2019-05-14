c7585248f5b1d8974c5e5ed33c466dce1700abe5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-05-14 11:15:20 +1000

    Add new resource budget sizes.

--- jupyterhub/.jupyter/config-learning-portal.py
+++ jupyterhub/.jupyter/config-learning-portal.py
@@ -440,9 +440,9 @@ resource_budget_mapping = {
             "spec": {
                 "hard": {
                     "persistentvolumeclaims": "3",
-                    "replicationcontrollers": "15",
-                    "secrets": "15",
-                    "services": "3"
+                    "replicationcontrollers": "10",
+                    "secrets": "20",
+                    "services": "5"
                 }
             }
         },
@@ -550,10 +550,10 @@ resource_budget_mapping = {
             },
             "spec": {
                 "hard": {
-                    "persistentvolumeclaims": "5",
-                    "replicationcontrollers": "20",
-                    "secrets": "20",
-                    "services": "5"
+                    "persistentvolumeclaims": "6",
+                    "replicationcontrollers": "15",
+                    "secrets": "25",
+                    "services": "10"
                 }
             }
         },
@@ -661,10 +661,10 @@ resource_budget_mapping = {
             },
             "spec": {
                 "hard": {
-                    "persistentvolumeclaims": "10",
-                    "replicationcontrollers": "30",
-                    "secrets": "30",
-                    "services": "10"
+                    "persistentvolumeclaims": "12",
+                    "replicationcontrollers": "25",
+                    "secrets": "35",
+                    "services": "20"
                 }
             }
         }
@@ -772,10 +772,232 @@ resource_budget_mapping = {
             },
             "spec": {
                 "hard": {
-                    "persistentvolumeclaims": "15",
-                    "replicationcontrollers": "40",
-                    "secrets": "40",
-                    "services": "15"
+                    "persistentvolumeclaims": "18",
+                    "replicationcontrollers": "35",
+                    "secrets": "45",
+                    "services": "30"
+                }
+            }
+        }
+    },
+    "xx-large": {
+        "resource-limits" : {
+            "kind": "LimitRange",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "resource-limits",
+                "annotations": {
+                    "resource-budget": "xx-large"
+                }
+            },
+            "spec": {
+                "limits": [
+                    {
+                        "type": "Pod",
+                        "min": {
+                            "cpu": "50m",
+                            "memory": "32Mi"
+                        },
+                        "max": {
+                            "cpu": "12",
+                            "memory": "12Gi"
+                        }
+                    },
+                    {
+                        "type": "Container",
+                        "min": {
+                            "cpu": "50m",
+                            "memory": "32Mi"
+                        },
+                        "max": {
+                            "cpu": "12",
+                            "memory": "12Gi"
+                        },
+                        "default": {
+                            "cpu": "500m",
+                            "memory": "2Gi"
+                        },
+                        "defaultRequest": {
+                            "cpu": "50m",
+                            "memory": "128Mi"
+                        }
+                    },
+                    {
+                        "type": "PersistentVolumeClaim",
+                        "min": {
+                            "storage": "1Gi"
+                        },
+                        "max": {
+                            "storage": "20Gi"
+                        }
+                    }
+                ]
+            }
+        },
+        "compute-resources" : {
+            "kind": "ResourceQuota",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "compute-resources",
+                "annotations": {
+                    "resource-budget": "xx-large"
+                }
+            },
+            "spec": {
+                "hard": {
+                    "limits.cpu": "12",
+                    "limits.memory": "12Gi"
+                },
+                "scopes": [
+                    "NotTerminating"
+                ]
+            }
+        },
+        "compute-resources-timebound" : {
+            "kind": "ResourceQuota",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "compute-resources-timebound",
+                "annotations": {
+                    "resource-budget": "xx-large"
+                }
+            },
+            "spec": {
+                "hard": {
+                    "limits.cpu": "12",
+                    "limits.memory": "12Gi"
+                },
+                "scopes": [
+                    "Terminating"
+                ]
+            }
+        },
+        "object-counts" : {
+            "kind": "ResourceQuota",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "object-counts",
+                "annotations": {
+                    "resource-budget": "xx-large"
+                }
+            },
+            "spec": {
+                "hard": {
+                    "persistentvolumeclaims": "24",
+                    "replicationcontrollers": "45",
+                    "secrets": "55",
+                    "services": "40"
+                }
+            }
+        }
+    },
+    "xxx-large": {
+        "resource-limits" : {
+            "kind": "LimitRange",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "resource-limits",
+                "annotations": {
+                    "resource-budget": "xxx-large"
+                }
+            },
+            "spec": {
+                "limits": [
+                    {
+                        "type": "Pod",
+                        "min": {
+                            "cpu": "50m",
+                            "memory": "32Mi"
+                        },
+                        "max": {
+                            "cpu": "16",
+                            "memory": "16Gi"
+                        }
+                    },
+                    {
+                        "type": "Container",
+                        "min": {
+                            "cpu": "50m",
+                            "memory": "32Mi"
+                        },
+                        "max": {
+                            "cpu": "16",
+                            "memory": "16Gi"
+                        },
+                        "default": {
+                            "cpu": "500m",
+                            "memory": "2Gi"
+                        },
+                        "defaultRequest": {
+                            "cpu": "50m",
+                            "memory": "128Mi"
+                        }
+                    },
+                    {
+                        "type": "PersistentVolumeClaim",
+                        "min": {
+                            "storage": "1Gi"
+                        },
+                        "max": {
+                            "storage": "20Gi"
+                        }
+                    }
+                ]
+            }
+        },
+        "compute-resources" : {
+            "kind": "ResourceQuota",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "compute-resources",
+                "annotations": {
+                    "resource-budget": "xxx-large"
+                }
+            },
+            "spec": {
+                "hard": {
+                    "limits.cpu": "16",
+                    "limits.memory": "16Gi"
+                },
+                "scopes": [
+                    "NotTerminating"
+                ]
+            }
+        },
+        "compute-resources-timebound" : {
+            "kind": "ResourceQuota",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "compute-resources-timebound",
+                "annotations": {
+                    "resource-budget": "xxx-large"
+                }
+            },
+            "spec": {
+                "hard": {
+                    "limits.cpu": "16",
+                    "limits.memory": "16Gi"
+                },
+                "scopes": [
+                    "Terminating"
+                ]
+            }
+        },
+        "object-counts" : {
+            "kind": "ResourceQuota",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "object-counts",
+                "annotations": {
+                    "resource-budget": "xxx-large"
+                }
+            },
+            "spec": {
+                "hard": {
+                    "persistentvolumeclaims": "30",
+                    "replicationcontrollers": "55",
+                    "secrets": "65",
+                    "services": "50"
                 }
             }
         }
